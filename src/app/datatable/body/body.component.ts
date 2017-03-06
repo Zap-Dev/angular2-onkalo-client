255a41d5e4fc1e93fb5498a4fe9f1f5e57887c29
Zap-Dev <pasquale.guarino80@gmail.com>
2017-03-06 19:05:59 +0100

    Filter - Pagination

--- src/app/datatable/body/body.component.ts
+++ src/app/datatable/body/body.component.ts
@@ -11,12 +11,13 @@ export class BodyComponent implements OnInit {
   @Input() products:  Product []
   @Input() fixedCols: any
   @Input() checked = false;
+  @Input() itemForPage:[{}];
+  selectedValue: string;
   indeterminate = false;
   align = 'start';
   disabled = false;
 
   username: string;
-  collection = [];
 
 
   toUpperCase () {
@@ -28,9 +29,17 @@ export class BodyComponent implements OnInit {
 
 
   constructor() {
-    for (let i = 1; i <= 100; i++) {
-      this.collection.push(`item ${i}`);
-    }
+    this.selectedValue="10"
+    this.itemForPage = [
+      {value:10, viewValue:"10"},
+      {value:25, viewValue:"25"},
+      {value:50, viewValue:"50"},
+      {value:100, viewValue:"100"},
+      {value:250, viewValue:"250"},
+      /*{value:500, viewValue:"500"},*/
+      /*{value:1000, viewValue:"1000"}*/
+      ]
+
   }
 
   ngOnInit() {
